39324a7376ab46bb11d657d47d1d471ed66dfde9
Vik Gardella <60987280+VikramGardella@users.noreply.github.com>
2021-11-13 15:51:32 -0500

    Roughest of drafts
    Will add back-end eventually as a "mock" local/offline SQL server, maybe Azure. Also will add the data classes for specific fields of patient health (see them all listed in multi-line comment in Patient.cs as member variables) as I have content to add to them. Ultimately will add a front-end as a desktop or web app and that's why I went with C# but subject to change; could be easily translated over to Java or C++ in a few minutes.

---
+++ Patient.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iHeal
+{
+    public class Patient
+    {
+        string firstName, lastName, address, gender;
+        int ageYears, heightInches, weightPounds, systolicBP, diastolicBP;
+        int dayDOB, monthDOB, yearDOB, heartRateResting, patientID;
+
+        List<visitData> visitsLog = new List<visitData>();
+
+        //add image for patient profile pic as member variable too
+
+        /*
+        dataCardiology cardioData = new dataCardiology();
+        dataNeurology neuroData = new dataDermatology();
+        dataPsychiatry pyschData = new dataPsychiatry();
+        dataOphthalmology ophthaData = new dataOphthalmology();
+        dataOrthopediatry orthoData = new dataOrthopediatry();
+        dataPathology pathoData = new dataPathology();
+        dataRadiology radioData = new dataRadiology();
+        dataAnesthesiology anesthData = new dataAnesthesiology();
+        dataOtolaryngology otolaData = new dataOtolaryngology();
+        dataUrology uroData = new dataUrology();
+        dataDermatology dermaData = new dataDermatology();
+         */
+
+
+        public Patient(string firstN, string lastN, int dDOB, int mDOB, int yDOB, string gen, int patID)
+        {
+            firstName = firstN;
+            lastName = lastN;
+            dayDOB = dDOB;
+            monthDOB = mDOB;
+            yearDOB = yDOB;
+            gender = gen;
+            patientID = patID;
+        }
+
+
+        //Accessors
+        public int getID() { return patientID; }
+
+        public int getHeight() { return heightInches; }
+
+        public int getWeight() { return weightPounds; }
+
+        public int getSystolic() { return systolicBP; }
+
+        public int getDiastolic() { return diastolicBP; }
+
+        public string getAsStringBP() {
+            return ("" + systolicBP + "/" + diastolicBP);
+        }
+
+        public int getRestingHR() { return heartRateResting; }
+
+
+        //Mutators
+        public void setHeight(int newH) { heightInches = newH; }
+
+        public void setWeight(int newW) { weightPounds = newW; }
+
+        public void setSystolic(int newSys) { systolicBP = newSys; }
+
+        public void setDiastolic(int newDia) { diastolicBP = newDia; }
+
+        public void setHeartRate(int newHR) { heartRateResting = newHR; }
+
+
+        //Functions
+        public double calcBMI() { return -1.0; }
+
+        public int calcAge() { return -1; } //use system call to get current date, then find Math.floor(difference)
+
+        public Boolean detectIssuesBP() {
+            if (1==1)
+            {
+                return false;
+            }
+            else {
+                Console.WriteLine("Warning, " + firstName + "'s blood pressure indicates a risk of...");
+                return true;
+            }
+
+        }
+
+        public void addVisitData(visitData vis) {
+            visitsLog.Add(vis);
+        }
+
+        public void printAllPatientData() {
+            Console.WriteLine("\n\n------------------------------\n");
+
+            Console.WriteLine("[" + lastName + ", " + firstName + "] - #" + patientID + "\n");
+            Console.WriteLine("-Generic Data:");
+            Console.WriteLine("\tDOB: [" + dayDOB + " / " + monthDOB + " / " + yearDOB + "]");
+            Console.WriteLine("\tGender: " + gender);
+            Console.WriteLine("\tHeight: " + heightInches + " Inches");
+            Console.WriteLine("\tWeight: " + weightPounds + " LBS");
+            Console.WriteLine("-Systemic Data:");
+            Console.WriteLine("\tBlood Pressure: " + systolicBP + "/" + diastolicBP);
+            Console.WriteLine("\tHeart Rate: " + heartRateResting + " BPM");
+
+            Console.WriteLine("\n-Cardiologic Data:");
+            //cardioData.printSummary();
+            Console.WriteLine("\n-Neurologic Data:");
+            //neuroData.printSummary();
+            Console.WriteLine("\n-Psychiatric Data:");
+            //psychData.printSummary();
+            Console.WriteLine("\n-Ophthalmic Data:");
+            //ophthaData.printSummary();
+            Console.WriteLine("\n-Orthopedic Data:");
+            //orthoData.printSummary();
+            Console.WriteLine("\n-Pathologic Data:");
+            //pathoData.printSummary();
+            Console.WriteLine("\n-Radiologic Data:");
+            //radioData.printSummary();
+            Console.WriteLine("\n-Anesthesiologic Data:");
+            //anesthData.printSummary();
+            Console.WriteLine("\n-Otolaryngologic Data:");
+            //otolaData.printSummary();
+            Console.WriteLine("\n-Urologic Data:");
+            //uroData.printSummary();
+            Console.WriteLine("\n-Dermatologic Data:");
+            //dermaData.printSummary();
+
+            Console.WriteLine("\n------------------------------\n\n");
+        }
+
+    }
+}
---
+++ visitData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace iHeal
+{
+    public class visitData
+    {
+        string dateOf, reason, outcome;
+        int patientID, heartRate, systolicBP, diastolicBP, height, weight;
+
+        public visitData(string dat, int patID, int HR, int sysBP, int diaBP, int h, int w, string rea, string outc) {
+            dateOf = dat;
+            patientID = patID;
+            heartRate = HR;
+            systolicBP = sysBP;
+            diastolicBP = diaBP;
+            height = h;
+            weight = w;
+            reason = rea;
+            outcome = outc;
+        }
+
+        public string getDate() { return dateOf; }
+
+        public string getReason() { return reason; }
+
+        public string getOutcome() { return outcome; }
+
+        public int getID() { return patientID; }
+
+        public int getHR() { return heartRate; }
+
+        public int getSysBP() { return systolicBP; }
+
+        public int getDiaBP() { return diastolicBP; }
+
+        public int getHeight() { return height; }
+
+        public int getWeight() { return weight; }
+
+
+    }
+}
